d86189a158ad09d36dbb7f608dccf38069ec63d5
Marcel <marcel.piotrowski5543@gmail.com>
2023-04-11 14:44:36 +0200

    feat: UI overhaul, UI initial project.

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ from forms import LoginForm
 
 # create the app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'h+u5-sNA2%Fr&3"y"9nQEn==rfLjfKB{$RGShJ"$2I`d&j[5-J79:RJZoQJ('
-#app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'
+app.config[
+    "SECRET_KEY"
+] = 'h+u5-sNA2%Fr&3"y"9nQEn==rfLjfKB{$RGShJ"$2I`d&j[5-J79:RJZoQJ('
+# app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'
 bootstrap = Bootstrap5(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/secret')
+@app.route("/")
+def index():
+    flash("Please input at least one DHCP entity description below.", "success")
+    return render_template("index.html")
+
+
+@app.route("/secret")
 def secret():
-    return render_template('secret.html')
+    return render_template("secret.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
