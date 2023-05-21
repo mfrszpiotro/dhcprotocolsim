ff2b62f2dfcb6a09d7dbe9d077a0964724aa5409
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-05-21 15:30:10 +0200

    feat: added testing case for message listen

--- app.py
+++ app.py
@@ -63,9 +63,18 @@ def simulation():
     
     p1 = Packet(1, 2, "there is no try")
     simulation.sendMessage(p1)
-    
+
     p2 = Packet(2, 1, "do or do not")
     simulation.sendMessage(p2)
+
+    p3 = Packet(2, 1, "or have a donut")
+    simulation.sendMessage(p3)
+
+    p4 = Packet(2, 1, "or be gone")
+    simulation.sendMessage(p4)
+
+    # Exemplary listen message
+    simulation.listenMessage(e1)
     
     flash(simulation.print(), "success")
     if request.method == "POST":
