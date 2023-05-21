36a6a2a0d6aa662ab6431ce4ce27c12d7992e9ed
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-05-21 14:07:53 +0200

    feat: core clases implementation

--- entity.py
+++ entity.py
@@ -1,13 +1,43 @@
 class Entity:
-    queue = {}
-    max_queue_length = 1
-
-    def init(self, max_queue_length):
+    def __init__(self, name, max_queue_length=1):
+        self.name = name
+        self.queue = []
         self.max_queue_length = max_queue_length
-        return
 
-    def send(self, message, destination):
-        return
+class Packet:
+    def __init__(self, source, destination, message):
+        self.source = source
+        self.destination = destination
+        self.message = message
 
-    def listen(self, message, source, wait_time=None):
-        return
+class Simulation:
+    def __init__(self):
+        self.entities = []
+
+    def addEntity(self, entity):
+        self.entities.append(entity)
+
+    def sendMessage(self, packet):
+        for entity in self.entities:
+            if entity.name == packet.destination:
+                entity.queue.append(packet.message)
+
+    #def listenMessage(self, entity): #wait_time=None):
+    #    while True:
+    #        print("works")
+    #        if entity.queue:
+    #            message = entity.queue.pop(0)
+    #            print(f"Recovered message: {message}")
+    #            break
+
+    def print(self):
+        list = []
+        for entity in self.entities:
+            list.append(f"Entity: {entity.name}, Queue: {entity.queue}")
+
+        return list
+
+    # while loop for send/listen execution, finised when no code is left to run
+    def exec(self):
+        pass
+
