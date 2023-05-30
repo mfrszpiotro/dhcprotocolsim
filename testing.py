1b1b85d3d18ee411dbe079c3952c926bab3f3c8a
aszweryn <101809061+aszweryn@users.noreply.github.com>
2023-05-30 14:37:15 +0200

    fix: working dhcp test case

--- testing.py
+++ testing.py
@@ -19,52 +19,48 @@ def test_dhcp():
     simulation.sendMessage(p11)
     simulation.sendMessage(p12)
     simulation.listenMessage(p11, e3)
-    # not sure if correct!
-    simulation.listenMessage(p12, e3)
+    # DECIDE ON THE IMPLEMENTATION IN THE LATER STAGE!
+    # simulation.listenMessage(p12, e3)
     simulation.checkFinish()
 
     # STEP 2
     utils.writer(simulation.logfile, "a", "STEP 2\n")
-    p21 = Packet(3, 1, "DHCP DISCOVER")
+    p21 = Packet(3, 1, "DHCP OFFER")
+    # REMEMBER THE ONE THAT HAS BEEN LISTENED IN THE STEP BEFORE
+    p22 = Packet(3, 2, "DHCP OFFER")
     simulation.sendMessage(p21)
-    simulation.listenMessage(p11, e1)
-    simulation.listenMessage(p12, e2)
+    simulation.listenMessage(p21, e1)
+    simulation.listenMessage(p22, e2)
+    # LOOK COMMENT ABOVE
+    # simulation.listenMessage(p22, e2)
     simulation.checkFinish()
 
     # STEP 3
     utils.writer(simulation.logfile, "a", "STEP 3\n")
     p31 = Packet(1, 3, "DHCP REQUEST")
+    # LOOK COMMENTS ABOVE
+    # p32 = Packet(2, 3, "DHCP REQUEST")
     simulation.sendMessage(p31)
+    # simulation.sendMessage(p32)
     simulation.listenMessage(p31, e3)
+    # simulation.listenMessage(p32, e3)
     simulation.checkFinish()
 
     # STEP 4
     utils.writer(simulation.logfile, "a", "STEP 4\n")
     p41 = Packet(3, 1, "DHCP ACKNOWLEDGE")
+    # LOOK COMMENTS ABOVE
+    # p42 = Packet(3, 2, "DHCP ACKNOWLEDGE")
     simulation.sendMessage(p41)
+    # simulation.sendMessage(p42)
     simulation.listenMessage(p41, e1)
+    # simulation.listenMessage(p42, e2)
     simulation.checkFinish()
 
     # STEP 5
     utils.writer(simulation.logfile, "a", "STEP 5\n")
-    # ENTITY 1 "END"
-    p51 = Packet(3, 2, "DHCP OFFER")
-    simulation.sendMessage(p51)
-
-    # STEP 6
-    utils.writer(simulation.logfile, "a", "STEP 6\n")
-    p61 = Packet(2, 3, "DHCP REQUEST")
-    simulation.sendMessage(p61)
-    simulation.listenMessage(p61, e3)
-
-    # STEP 7
-    utils.writer(simulation.logfile, "a", "STEP 7\n")
-    p71 = Packet(3, 2, "DHCP ACKNOWLEDGE")
-    simulation.sendMessage(p71)
-    simulation.listenMessage(p71, e3)
-
-    # STEP 8
-    # ENTITY 2 "END"
+    utils.writer(simulation.logfile, "a", "ENTITY 1: END")
+    # ENTITY 2 END
 
 def test_handshake():
     e1 = Entity(1)
