4f21da7dc95963116167312d3c05257ada185307
Marcel <82543883+mfrszpiotro@users.noreply.github.com>
2023-06-02 15:31:43 +0200

    Step by step mode (#10)
    * feat: step by step mode page introduced
    
    * feat: slider with simulation log introduced
    
    * feat: commands from step outputted in simple list
    
    * feat: blocking input implemented after step
    
    * chore: fix log position

--- app.py
+++ app.py
@@ -16,12 +16,60 @@ bootstrap = Bootstrap5(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    session.pop("descriptions", None)
+    session.pop("log", None)
     if request.method == "POST":
         if request.form.get("start"):
+            session["descriptions"] = [create_triplet("", 1)]
+            session["log"] = "start point log "
+            session["step_number"] = 1
+            session["blocked"] = []
+            return redirect(url_for("step"))
+
+        if request.form.get("alt"):
             session["descriptions"] = [create_triplet("", 4)]
             return redirect(url_for("define"))
 
     return render_template("index.html")
+
+
+@app.route("/step", methods=["GET", "POST"])
+def step():
+    step_number = session.get("step_number")
+    blocked = session.get("blocked")
+    if request.method == "POST":
+        if request.form.get("simulate"):
+            session.pop("_flashes", None)
+            commands = [v for k, v in request.form.items() if k.startswith("e") and k.endswith(str(step_number))]
+            # commands to be translated into full simulation step execution
+            wasSimulated = True
+            # descriptions to be blocked if entity is deadlocked in this step
+            if commands and wasSimulated:
+                session["step_number"] = step_number+1
+                # data based on the output from the simulation:
+                session["log"] = session.get("log") + "\n" + str(commands)
+                session["blocked"] = [2]
+            return redirect(url_for("step"))
+
+        if request.form.get("add"):
+            session.pop("_flashes", None)
+            descr = list(session.get("descriptions"))
+            descr.append(create_triplet("", 1))
+            session["descriptions"] = descr
+
+        if request.form.get("save"):
+            return redirect(url_for("simulation"))
+
+        if request.form.get("back"):
+            return redirect(url_for("index"))
+
+    return render_template(
+        "step.html",
+        page_descriptions=session.get("descriptions")[1:],
+        first_triplet=session.get("descriptions")[0],
+        step_number=step_number,
+        blocked=blocked
+    )
 
 
 @app.route("/define", methods=["GET", "POST"])
@@ -41,7 +89,9 @@ def define():
 
         if request.form.get("add"):
             session.pop("_flashes", None)
-            session["descriptions"].append(create_triplet("", 4))
+            descr = list(session.get("descriptions"))
+            descr.append(create_triplet("", 4))
+            session["descriptions"] = descr
 
         if request.form.get("back"):
             return redirect(url_for("index"))
@@ -55,6 +105,10 @@ def define():
 
 @app.route("/define/simulation", methods=["GET", "POST"])
 def simulation():
+    log = session.get("log", None)
+    if log:
+        flash(log, "success")
+
     if request.method == "POST":
         if request.form.get("back"):
             return redirect(url_for("index"))
